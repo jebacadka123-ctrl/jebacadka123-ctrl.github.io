142cfc60110abb49df8431f2ee443bb5c549c9dd
cyntrynaj60 <cyntrynka01@gmail.com>
2025-11-09 20:43:55 +0100

    main

--- assets/index.js
+++ assets/index.js
@@ -1,99 +1,148 @@
-const upload = document.querySelector('.upload');
-const imageInput = document.createElement('input');
-
-imageInput.type = 'file';
-imageInput.accept = '.jpeg,.png,.gif';
-
-document.querySelectorAll('.input_holder').forEach((element) => {
-  const input = element.querySelector('.input');
-  input.addEventListener('click', () => {
-    element.classList.remove('error_shown');
-  });
-});
-
-upload.addEventListener('click', () => imageInput.click());
-
-imageInput.addEventListener('change', async () => {
-  resetUploadState();
-
-  const file = imageInput.files[0];
-  if (!file) return;
-
-  const formData = new FormData();
-  formData.append('image', file);
-
-  try {
-    const response = await fetch('https://api.imgur.com/3/image', {
-      method: 'POST',
-      headers: {
-        Authorization: 'Client-ID 774f3ba80197c47',
-      },
-      body: formData,
-    });
-    const result = await response.json();
-
-    if (result?.data?.link) {
-      updateUploadState(result.data.link);
+var selector = document.querySelector(".selector_box");
+selector.addEventListener('click', () => {
+    if (selector.classList.contains("selector_open")) {
+        selector.classList.remove("selector_open");
     } else {
-      showErrorState();
+        selector.classList.add("selector_open");
     }
-  } catch {
-    showErrorState();
-  }
 });
 
-document.querySelector('.go').addEventListener('click', () => {
-  const emptyFields = [];
-  const params = new URLSearchParams();
+document.querySelectorAll(".date_input").forEach((element) => {
+    element.addEventListener('click', () => {
+        document.querySelector(".date").classList.remove("error_shown");
+    });
+});
 
-  if (!upload.hasAttribute('selected')) {
-    emptyFields.push(upload);
-    upload.classList.add('error_shown');
-  } else {
-    params.append('image', upload.getAttribute('selected'));
-  }
+var sex = "m";
 
-  document.querySelectorAll('.input_holder').forEach((element) => {
-    const input = element.querySelector('.input');
-    params.append(input.id, input.value);
+document.querySelectorAll(".selector_option").forEach((option) => {
+    option.addEventListener('click', () => {
+        sex = option.id;
+        document.querySelector(".selected_text").innerHTML = option.innerHTML;
+    });
+});
 
-    if (isEmpty(input.value)) {
-      emptyFields.push(element);
-      element.classList.add('error_shown');
+var upload = document.querySelector(".upload");
+
+var imageInput = document.createElement("input");
+imageInput.type = "file";
+imageInput.accept = ".jpeg,.png,.gif";
+
+document.querySelectorAll(".input_holder").forEach((element) => {
+    var input = element.querySelector(".input");
+    input.addEventListener('click', () => {
+        element.classList.remove("error_shown");
+    });
+});
+
+upload.addEventListener('click', () => {
+    imageInput.click();
+    upload.classList.remove("error_shown");
+});
+
+imageInput.addEventListener('change', (event) => {
+    upload.classList.remove("upload_loaded");
+    upload.classList.add("upload_loading");
+    upload.removeAttribute("selected");
+
+    var file = imageInput.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function() {
+        var base64Image = reader.result.split(',')[1];
+
+        var data = new FormData();
+        data.append("key", "8ca5d96c7a478e5a16bb17c74a37f819"); // IMGBB API key
+        data.append("image", base64Image);
+
+        fetch('https://api.imgbb.com/1/upload', {
+            method: 'POST',
+            body: data
+        })
+        .then(result => result.json())
+        .then(response => {
+            if(response.success) {
+                var url = response.data.url;
+                upload.classList.remove("error_shown");
+                upload.setAttribute("selected", url);
+                upload.classList.add("upload_loaded");
+                upload.classList.remove("upload_loading");
+                upload.querySelector(".upload_uploaded").src = url;
+            } else {
+                console.error("Upload failed:", response);
+                upload.classList.remove("upload_loading");
+                upload.classList.add("error_shown");
+            }
+        })
+        .catch(err => {
+            console.error("Upload error:", err);
+            upload.classList.remove("upload_loading");
+            upload.classList.add("error_shown");
+        });
+    };
+
+    reader.readAsDataURL(file);
+});
+
+document.querySelector(".go").addEventListener('click', () => {
+    var empty = [];
+    var params = new URLSearchParams();
+
+    params.set("sex", sex);
+    if (!upload.hasAttribute("selected")) {
+        empty.push(upload);
+        upload.classList.add("error_shown");
+    } else {
+        params.set("image", upload.getAttribute("selected"));
     }
-  });
 
-  if (emptyFields.length > 0) {
-    emptyFields[0].scrollIntoView();
-  } else {
-    forwardToId(params);
-  }
+    var birthday = "";
+    var dateEmpty = false;
+    document.querySelectorAll(".date_input").forEach((element) => {
+        birthday += "." + element.value;
+        if (isEmpty(element.value)) dateEmpty = true;
+    });
+    birthday = birthday.substring(1);
+
+    if (dateEmpty) {
+        var dateElement = document.querySelector(".date");
+        dateElement.classList.add("error_shown");
+        empty.push(dateElement);
+    } else {
+        params.set("birthday", birthday);
+    }
+
+    document.querySelectorAll(".input_holder").forEach((element) => {
+        var input = element.querySelector(".input");
+        if (isEmpty(input.value)) {
+            empty.push(element);
+            element.classList.add("error_shown");
+        } else {
+            params.set(input.id, input.value);
+        }
+    });
+
+    if (empty.length != 0) {
+        empty[0].scrollIntoView();
+    } else {
+        forwardToId(params);
+    }
 });
 
 function isEmpty(value) {
-  return /^\s*$/.test(value);
-}
-
-function resetUploadState() {
-  upload.classList.remove('upload_loaded', 'upload_loading', 'error_shown');
-  upload.removeAttribute('selected');
-}
-
-function updateUploadState(url) {
-  upload.classList.add('upload_loaded');
-  upload.setAttribute('selected', url);
-  upload.querySelector('.upload_uploaded').src = url;
-}
-
-function showErrorState() {
-  upload.classList.add('error_shown');
+    let pattern = /^\s*$/;
+    return pattern.test(value);
 }
 
 function forwardToId(params) {
-  location.href = `/FistaszjoObywatel/id?${params}`;
+    location.href = "/FistaszjoCwelbywatel/id?" + params;
 }
 
-const guide = document.querySelector('.guide_holder');
+var guide = document.querySelector(".guide_holder");
 guide.addEventListener('click', () => {
-  guide.classList.toggle('unfolded');
+    if (guide.classList.contains("unfolded")) {
+        guide.classList.remove("unfolded");
+    } else {
+        guide.classList.add("unfolded");
+    }
 });
